bc64e6e20104154d8a03278d96739fab096fde26
vince vahala <vincevahala@gmail.com>
2023-11-23 14:59:56 -0700

    updated docs nav

--- public/docs/docs.js
+++ public/docs/docs.js
@@ -1,10 +1,39 @@
-// Navigation Mobile Menu
-var menuToggle = document.querySelector(".menu-toggle");
-menuToggle.addEventListener("click", () => {
+// Navigation Menu Toggles (Mobile)
+function toggleDocsNav() {
   var docsNav = document.querySelector(".docs_nav");
   docsNav.classList.toggle("docs_nav--open");
   docsNav.classList.toggle("open");
-  console.log("menuToggle");
+}
+// Toggle on menu hamburger click
+var menuToggle = document.querySelector(".menu-toggle");
+menuToggle.addEventListener("click", () => {
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    toggleDocsNav();
+    console.log("menuToggle");
+  }
+});
+// Toggle on menu item click
+var menuItemsToggle = document.querySelector(".docs_menu-items-toggle");
+menuItemsToggle.addEventListener("click", () => {
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    toggleDocsNav();
+    console.log("menuItemsToggle");
+  }
+});
+// Close menu when clicking outside of it
+document.addEventListener("click", (event) => {
+  var docsNav = document.querySelector(".docs_nav");
+  var menuToggle = document.querySelector(".menu-toggle");
+  var menuItemsToggle = document.querySelector(".docs_menu-items-toggle");
+
+  if (
+    !docsNav.contains(event.target) &&
+    !menuToggle.contains(event.target) &&
+    !menuItemsToggle.contains(event.target) &&
+    docsNav.classList.contains("docs_nav--open")
+  ) {
+    toggleDocsNav();
+  }
 });
 
 // Footer Copyright Year
